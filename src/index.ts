cb97501027e5c63f13d41be718a155fbf97b721f
bunsal.infinity <bunsal141.infinity@gmail.com>
2024-06-03 07:57:21 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import knex from 'knex';
 import knexConfig from './database/knexfile';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.DB_PORT || 5000;
 
 const db = knex(knexConfig.development);
 
